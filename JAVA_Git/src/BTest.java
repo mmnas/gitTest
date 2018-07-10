16818d7e7190851a8650b899f087ba87629dbfc3
kosmo17 <kosmo17@kosmo17-PC>
2018-07-10 12:19:56 +0900

    BTest 세 번째 커미뜨 : B사용자 출력문 추가

--- JAVA_Git/src/BTest.java
+++ JAVA_Git/src/BTest.java
@@ -3,5 +3,6 @@ public class BTest {
 	public static void main(String args[]) {
 		System.out.println("BTest 첫 번째 커미뜨");
 		System.out.println("BTest 두 번째 커미뜨 : Made In A");
+		System.out.println("BTest 세 번째 커미뜨 : B사용자 출력문 추가");
 	}
 }
